19afe9704a2421e121cf683db8227eab5b818854
megan <mtpott23@gmail.com>
2022-04-12 10:22:52 -0400

    candidates objects appear in server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,23 @@ const app = express();
 //express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your SQL username
+        user: 'root',
+        //your SQL password
+        password: 'root',
+        database: 'election'
+    },
+    console.log('connected to the election database')
+)
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //default response for any other request (not found)
 app.use((req, res) => {
